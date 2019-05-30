695ea53a298893e5ef79ffc7135935f3b4706876
Jason Leach <jlspursfan@gmail.com>
2019-05-30 13:55:17 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,6 @@
 def alphabetize(arr)
   esp_alph = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  arr.sort_by {|string|  
+  arr.sort_by {|string|
     [esp_alph.index(string[0]),
      esp_alph.index(string[1]),
      esp_alph.index(string[2]),
